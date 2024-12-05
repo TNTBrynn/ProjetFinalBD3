58b968d5ac9ef9773aeb18ecfee34055095e5149
SnowB0mb <patrysimon@videotron.ca>
2024-12-05 14:03:11 -0500

    modification sous transaction

--- ProjetBD3Final/ProjetBD3Final/MajAbonne.cs
+++ ProjetBD3Final/ProjetBD3Final/MajAbonne.cs
@@ -14,9 +14,12 @@ namespace ProjetBD3Final
 {
     public partial class MajAbonne : Form
     {
+        private DataClassesDataContext dataContext = new DataClassesDataContext();
+
         public MajAbonne()
         {
             InitializeComponent();
+            dataContext.Connection.Open();
         }
 
         private void abonnementsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
@@ -43,6 +46,7 @@ namespace ProjetBD3Final
         private void dgAbonnements_RowValidating(object sender, DataGridViewCellCancelEventArgs e)
         {
             string messageErreur = string.Empty;
+            string id = dgAbonnements["dgTxtId", e.RowIndex].Value?.ToString();
             string prenom = dgAbonnements["dgTxtPrenom", e.RowIndex].Value?.ToString();
             string sexe = dgAbonnements["dgTxtSexe", e.RowIndex].Value?.ToString();
             string noCivique = dgAbonnements["dgTxtNoCivique", e.RowIndex].Value?.ToString();
@@ -51,7 +55,9 @@ namespace ProjetBD3Final
             string idProvince = dgAbonnements["dgTxtIdProvince", e.RowIndex].Value?.ToString();
             string codePostal = dgAbonnements["dgTxtCodePostal", e.RowIndex].Value?.ToString();
             string telephone = dgAbonnements["dgTxtTelephone", e.RowIndex].Value?.ToString();
+            string cellulaire = dgAbonnements["dgTxtCellulaire", e.RowIndex].Value?.ToString();
             string courriel = dgAbonnements["dgTxtCourriel", e.RowIndex].Value?.ToString();
+            string remarque = dgAbonnements["dgTxtRemarque", e.RowIndex].Value?.ToString();
 
             var regexTelephone = new System.Text.RegularExpressions.Regex(@"^\d{10}$");
             var regexCodePostal = new System.Text.RegularExpressions.Regex(@"^[A-Za-z]\d[A-Za-z]\d[A-Za-z]\d$");
@@ -78,6 +84,24 @@ namespace ProjetBD3Final
             {
                 messageErreur = "Le sexe doit être H ou F";
             }
+            else
+            {
+                var editAbonnement = (from abonnement in dataContext.Abonnements
+                                 where abonnement.Id == id
+                                 select abonnement).First();
+
+                editAbonnement.Prenom = prenom;
+                editAbonnement.Sexe = Convert.ToChar(sexe);
+                editAbonnement.NoCivique = Convert.ToInt32(noCivique);
+                editAbonnement.Rue = rue;
+                editAbonnement.Ville = ville;
+                editAbonnement.IdProvince = idProvince;
+                editAbonnement.CodePostal = codePostal;
+                editAbonnement.Telephone = telephone;
+                editAbonnement.Cellulaire = cellulaire;
+                editAbonnement.Courriel = courriel;
+                editAbonnement.Remarque = remarque;
+            }
 
             dgAbonnements.Rows[e.RowIndex].ErrorText = messageErreur;
             if (messageErreur != string.Empty)
@@ -99,9 +123,11 @@ namespace ProjetBD3Final
         private void dgDependants_RowValidating(object sender, DataGridViewCellCancelEventArgs e)
         {
             string messageErreur = string.Empty;
+            string id = dgDependants["dgTxtIdDependant", e.RowIndex].Value?.ToString();
             string nom = dgDependants["dgTxtNomDependant", e.RowIndex].Value?.ToString();
             string prenom = dgDependants["dgTxtPrenomDependant", e.RowIndex].Value?.ToString();
             string sexe = dgDependants["dgTxtSexeDependant", e.RowIndex].Value?.ToString();
+            string remarque = dgDependants["dgTxtRemarqueDependant", e.RowIndex].Value?.ToString();
 
 
             if (String.IsNullOrEmpty(prenom) || String.IsNullOrEmpty(sexe) || String.IsNullOrEmpty(nom))
@@ -111,6 +137,16 @@ namespace ProjetBD3Final
             else if (!(sexe == "H" || sexe == "F"))
             {
                 messageErreur = "Le sexe doit être H ou F";
+            }
+            else
+            {
+                var editDependant = (from dependant in dataContext.Dependants
+                                     where dependant.Nom == nom && dependant.Prenom == prenom
+                                     select dependant).First();
+                editDependant.Nom = nom;
+                editDependant.Prenom = prenom;
+                editDependant.Sexe = Convert.ToChar(sexe);
+                editDependant.Remarque = remarque;
             }
 
             dgDependants.Rows[e.RowIndex].ErrorText = messageErreur;
@@ -122,12 +158,26 @@ namespace ProjetBD3Final
 
         private void btnAnnuler_Click(object sender, EventArgs e)
         {
+            dataContext.Connection.Close();
             this.Close();
         }
 
         private void btnEnregistrer_Click(object sender, EventArgs e)
         {
-
+            using (var transaction = dataContext.Connection.BeginTransaction())
+            {
+                try
+                {
+                    dataContext.Transaction = transaction;
+                    dataContext.SubmitChanges();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    MessageBox.Show("Une erreur s'est produite lors de l'enregistrement des données : " + ex.Message);
+                }
+            }
         }
     }
 }
